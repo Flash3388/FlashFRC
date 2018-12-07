9154feb2de601659bfa3b70b213934fff6898217
Tom Tzook <tomtzook@gmail.com>
2018-12-07 23:07:05 +0200

    added frc io/devices implementations

---
+++ src/main/java/edu/flash3388/flashlib/frc/robot/io/FrcPwm.java
@@ -0,0 +1,61 @@
+package edu.flash3388.flashlib.frc.robot.io;
+
+import edu.flash3388.flashlib.robot.io.Pwm;
+import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.SensorBase;
+import edu.wpi.first.wpilibj.hal.DIOJNI;
+
+public class FrcPwm implements Pwm {
+
+    public static final int MAX_RAW = 255;
+
+    private edu.wpi.first.wpilibj.PWM mPwm;
+
+    public FrcPwm(PWM pwm) {
+        mPwm = pwm;
+    }
+
+    public FrcPwm(int port) {
+        this(new edu.wpi.first.wpilibj.PWM(port));
+    }
+
+    @Override
+    public void setDuty(double duty) {
+        setRaw((int) (duty * MAX_RAW));
+    }
+
+    @Override
+    public void setRaw(int raw) {
+        mPwm.setRaw(raw);
+    }
+
+    @Override
+    public double getDuty() {
+        return getRaw() / (double) MAX_RAW;
+    }
+
+    @Override
+    public int getRaw() {
+        return mPwm.getRaw();
+    }
+
+    @Override
+    public void setFrequency(double frequency) {
+        throw new UnsupportedOperationException("cannot set frequency");
+    }
+
+    @Override
+    public double getFrequency() {
+        return SensorBase.kSystemClockTicksPerMicrosecond * 1e3 / DIOJNI.getLoopTiming();
+    }
+
+    @Override
+    public void free() {
+        if (mPwm == null) {
+            return;
+        }
+
+        mPwm.free();
+        mPwm = null;
+    }
+}
