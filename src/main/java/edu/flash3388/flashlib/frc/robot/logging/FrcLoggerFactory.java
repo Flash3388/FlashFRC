9f018c2f803fbb1d8654ad50e734b4b16acc8b36
Tom Tzook <tomtzook@gmail.com>
2019-01-14 00:28:10 +0200

    updated logging to use builder-based logging

--- src/main/java/edu/flash3388/flashlib/frc/robot/logging/FrcLoggerFactory.java
+++ src/main/java/edu/flash3388/flashlib/frc/robot/logging/FrcLoggerFactory.java
@@ -1,31 +1,39 @@
 package edu.flash3388.flashlib.frc.robot.logging;
 
-import edu.flash3388.flashlib.util.logging.Logging;
+import edu.flash3388.flashlib.util.logging.LogBuildException;
+import edu.flash3388.flashlib.util.logging.LoggerBuilder;
 import edu.flash3388.flashlib.util.logging.jul.JulLoggerAdapter;
 import edu.wpi.first.wpilibj.DriverStation;
 import org.slf4j.Logger;
 
 import java.io.File;
-import java.io.IOException;
 
 public class FrcLoggerFactory {
 
     private static final String FRC_USER_DIRECTORY = "/home/lvuser";
+    private static final String LOGGER_NAME = "robot";
 
     private FrcLoggerFactory() {}
 
     public static Logger createLogger() {
-        java.util.logging.Logger logger;
-
-        try {
-            logger = Logging.getJulLogger(new File(FRC_USER_DIRECTORY), "robot");
-        } catch (IOException e) {
-            DriverStation.reportError(String.format("Error while initializing logger: %s", e.getMessage()), false);
-            logger = Logging.getJulConsoleLogger("robot");
-        }
-
+        java.util.logging.Logger logger = createBaseLogger();
         logger.addHandler(new DriverStationLoggerHandler());
 
         return new JulLoggerAdapter(logger);
+    }
+
+    private static java.util.logging.Logger createBaseLogger() {
+        try {
+            return new LoggerBuilder(LOGGER_NAME)
+                    .enableFileLogging(true)
+                    .setDateBasedFilesParent(new File(FRC_USER_DIRECTORY))
+                    .setTimeBasedFilePattern()
+                    .buildJul();
+        } catch (LogBuildException e) {
+            DriverStation.reportError("error creating log-based logger: " + e.getMessage(), false);
+            return new LoggerBuilder(LOGGER_NAME)
+                    .enableConsoleLogging(true)
+                    .buildJul();
+        }
     }
 }
