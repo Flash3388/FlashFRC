bb7e35116fa12437fcda4c2bf952e670b018d5af
Tom Tzook <tomtzook@gmail.com>
2020-08-25 17:33:16 +0300

    more examples

---
+++ examples/multisystem/src/main/java/frc/team3388/robot/RobotMap.java
@@ -0,0 +1,16 @@
+package frc.team3388.robot;
+
+import com.flash3388.flashlib.frc.robot.RoboRio;
+import com.flash3388.flashlib.hid.HidChannel;
+
+public class RobotMap {
+
+    public static final int DRIVE_FRONT_RIGHT = 0;
+    public static final int DRIVE_BACK_RIGHT = 1;
+    public static final int DRIVE_FRONT_LEFT = 2;
+    public static final int DRIVE_BACK_LEFT = 3;
+    public static final int SHOOTER = 4;
+
+    public static final HidChannel STICK_RIGHT = RoboRio.newHidChannel(0);
+    public static final HidChannel STICK_LEFT = RoboRio.newHidChannel(1);
+}
