49df4ee34ca3bc637cfb915683dd22f826498485
Tom Tzook <tomtzook@gmail.com>
2020-08-26 04:35:22 +0300

    added pnuematics

---
+++ flashlib.frc.robot/src/main/java/com/flash3388/flashlib/frc/robot/io/devices/pneumatics/FrcDoubleSolenoid.java
@@ -0,0 +1,44 @@
+package com.flash3388.flashlib.frc.robot.io.devices.pneumatics;
+
+import com.flash3388.flashlib.io.devices.pneumatics.DoubleSolenoid;
+
+public class FrcDoubleSolenoid implements DoubleSolenoid {
+
+    private final edu.wpi.first.wpilibj.DoubleSolenoid mSolenoid;
+
+    public FrcDoubleSolenoid(edu.wpi.first.wpilibj.DoubleSolenoid solenoid) {
+        mSolenoid = solenoid;
+    }
+
+    @Override
+    public void set(Value value) {
+        switch (value) {
+            case FORWARD:
+                mSolenoid.set(edu.wpi.first.wpilibj.DoubleSolenoid.Value.kForward);
+                break;
+            case REVERSE:
+                mSolenoid.set(edu.wpi.first.wpilibj.DoubleSolenoid.Value.kReverse);
+                break;
+            case OFF:
+                mSolenoid.set(edu.wpi.first.wpilibj.DoubleSolenoid.Value.kOff);
+                break;
+            default:
+                throw new AssertionError("unknown value");
+        }
+    }
+
+    @Override
+    public Value get() {
+        edu.wpi.first.wpilibj.DoubleSolenoid.Value value = mSolenoid.get();
+        switch (value) {
+            case kOff:
+                return Value.OFF;
+            case kForward:
+                return Value.FORWARD;
+            case kReverse:
+                return Value.REVERSE;
+            default:
+                throw new AssertionError("unknown value");
+        }
+    }
+}
---
+++ flashlib.frc.robot/src/main/java/com/flash3388/flashlib/frc/robot/io/devices/pneumatics/FrcSolenoid.java
@@ -0,0 +1,28 @@
+package com.flash3388.flashlib.frc.robot.io.devices.pneumatics;
+
+import com.flash3388.flashlib.io.devices.pneumatics.Solenoid;
+import com.flash3388.flashlib.time.Time;
+
+public class FrcSolenoid implements Solenoid {
+
+    private final edu.wpi.first.wpilibj.Solenoid mSolenoid;
+
+    public FrcSolenoid(edu.wpi.first.wpilibj.Solenoid solenoid) {
+        mSolenoid = solenoid;
+    }
+
+    @Override
+    public void set(boolean on) {
+        mSolenoid.set(on);
+    }
+
+    @Override
+    public boolean get() {
+        return mSolenoid.get();
+    }
+
+    @Override
+    public void pulse(Time duration) {
+        mSolenoid.setPulseDuration(duration.valueAsSeconds());
+    }
+}
